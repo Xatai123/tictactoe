2d1c5ae8ea5c70926b4e665195f001ebb546429c
Xatai123 <xataialiyev5@gmail.com>
2021-10-18 19:48:53 +0400

    turqay gay

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="mt-16 flex justify-center flex-wrap">
+    <div class="w-1/3 player">Player 1</div>
+    <div class="w-1/3 container mx-auto">
+      <div class="grid grid-cols-3">
+        <template v-for="m in 3">
+          <div
+            v-for="n in 3"
+            :key="(m - 1) * 3 + n"
+            :class="classGenerator((m - 1) * 3 + n)"
+            class="cursor-pointer border-black h-32 full-center my-font"
+            @click="makeMove(m, n)"
+          >
+            {{ matrix[m - 1][n - 1] }}
+          </div>
+        </template>
+      </div>
+    </div>
+    <div class="w-1/3 player">Player 2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPlayerOneTurn: true,
+      matrix: new Array(3).fill().map(() => new Array(3).fill("")),
+    };
+  },
+  watch: {
+    matrix: {
+      handler: function (val) {
+        console.log("ATC");
+        console.log(val);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    classGenerator(k) {
+      let res = "";
+      if (k < 4) res += "border-b-2";
+      else if (k > 6) res += "border-t-2";
+      if (k % 3 == 1) res += " border-r-2";
+      else if (k % 3 == 0) res += " border-l-2";
+      return res;
+    },
+    makeMove(m, n) {
+      m--;
+      n--;
+      if (this.matrix[m][n] == "") {
+        if (this.isPlayerOneTurn) this.matrix[m][n] = "x";
+        else this.matrix[m][n] = "o";
+        this.isPlayerOneTurn = !this.isPlayerOneTurn;
+        this.$forceUpdate();
+        this.isOver();
+      }
+    },
+    isOver() {
+      if (
+        this.matrix[0][0] == this.matrix[0][1] &&
+        this.matrix[0][0] == this.matrix[0][2]
+      )
+        return this.matrix[0][0];
+      else if (
+        this.matrix[1][0] == this.matrix[1][1] &&
+        this.matrix[1][0] == this.martix[1][2]
+      )
+        return this.matrix[1][0];
+      //   else if (
+      //     this.matrix[2][0] == this.matrix[2][1] &&
+      //     this.matrix[2][0] == this.martix[2][2]
+      //   )
+      //     return this.matrix[2][0];
+      //   else if (
+      //     this.matrix[0][0] == this.matrix[1][0] &&
+      //     this.matrix[0][0] == this.martix[2][0]
+      //   )
+      //     return this.matrix[0][0];
+      //   else if (
+      //     this.matrix[0][1] == this.matrix[1][1] &&
+      //     this.matrix[0][1] == this.martix[2][1]
+      //   )
+      //     return this.matrix[0][1];
+      //   else if (
+      //     this.matrix[0][2] == this.matrix[1][2] &&
+      //     this.matrix[0][2] == this.martix[2][2]
+      //   )
+      //     return this.matrix[0][2];
+      //   else if (
+      //     this.matrix[0][0] == matrix[1][1] &&
+      //     this.matrix[0][0] == this.martix[2][2]
+      //   )
+      //     return this.matrix[0][0];
+      //   else if (
+      //     this.matrix[2][0] == this.matrix[1][1] &&
+      //     this.matrix[2][0] == this.martix[0][2]
+      //   )
+      //     return this.matrix[2][0];
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <TicTacToe msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TicTacToe from "../components/TicTacToe.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    TicTacToe,
+  },
+};
 </script>
+
+<style>
+#logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
